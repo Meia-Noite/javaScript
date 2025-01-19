525b0de59dee43b7cbd45b6bdb5dcc6892153d1c
Natália Duarte <duarte.natalia@outlook.com.br>
2025-01-19 19:40:16 -0300

    Aula 05
    DOM
    criei um conversor muito básico de Real para Dólar

---
+++ aula005/aula005.js
@@ -0,0 +1,9 @@
+
+const converter = document.querySelector("input")
+document.querySelector('h1').style.color = 'blue'
+
+function btn(){
+    const valorConvertido = converter.value / 5
+
+    alert(` O valor em dólar é ${valorConvertido} Dólares`)
+}
